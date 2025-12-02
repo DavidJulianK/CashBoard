013750bf4ac18c44b97701918285959d49a17fe4
DavidJulianK <da.kiedacz@googlemail.com>
2025-12-02 14:14:00 +0100

    Setter und Getter für ID hinzugefügt

--- src/main/java/Cashboard/Backend/Transaction.java
+++ src/main/java/Cashboard/Backend/Transaction.java
@@ -26,6 +26,14 @@ public class Transaction {
         this.description = description;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public LocalDate getDate() {
         return date;
     }
